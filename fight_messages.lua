9431028fa143395e0d4056734fd7c616908399fe
Motleyii <102349191+Motleyii@users.noreply.github.com>
2022-03-25 07:48:59 +0000

    Initial draft tables
    These are the room/exit descriptions, objects, NPC's, fight messages and help messages

---
+++ fight_messages.lua
@@ -0,0 +1,41 @@
+--The @ in these messages is replaced by either the players name or "the NPC"
+
+fight_messages = {
+"You launch your attack with tremendous ferocity!",
+"You duck a badly misjudged blow and strike @ with all your might!",
+"@ almost knocks you out with a crushing blow!",
+"You duck a badly misjudged blow and strike @ with all your might!",
+"@ presses home the attack with savage fury!",
+"@ tries in vain to avoid your expert attack!",
+"@ presses home the attack with savage fury!",
+"You warily circle @ then lash out with a stunning blow!",
+"@ makes a vicious slash at your head!",
+"@ snarls and swings wildly at your head.",
+"@ presses home the attack with savage fury!",
+"@ staggers backwards as you thump it heavily!",
+"@ rushes in, hacking crazily!",
+"Taking the offensive you batter @ with a series of heavy blows!",
+"@ snarls and swings wildly at your head.",
+"@ staggers backwards as you thump it heavily!",
+"You leap into the fray and smash @ backwards a few feet!",
+"@ almost knocks you out with a crushing blow!",
+"@ snarls and swings wildly at your head.",
+"@ makes a vicious slash at your head!",
+"@ tries in vain to avoid your expert attack!",
+"@ makes a vicious slash at your head!",
+"There is nowhere for @ to hide as your attack forces it back!",
+"@ rushes in, hacking crazily!",
+"@ staggers backwards as you thump it heavily!",
+"@ lands a blow.....you reel dizzily!",
+"@ slips past your defence and lands a fearsome blow!",
+"@ slips past your defence and lands a fearsome blow!",
+"@ tries in vain to avoid your expert attack!",
+"@ lands a blow.....you reel dizzily!",
+"You leap into the fray and smash @ backwards a few feet!",
+-- Player flees
+"Suddenly, there's a brilliant flash, and a golden cloud of @ stars surround you before slowly winking out one by one...\nThe fight ceases....",
+-- Player wins
+"@ couldn't take your last blow and collapses to the ground. You win!\n    Suddenly an old man materialises and bends over the corpse. He shakes\nhis head in sorrow and scribbles against a name in a thick ledger\nhe is carrying.\n    The old man turns the pages until he finds your name. He thinks for\na moment and makes an adjustment to your score....he looks at you\nwith some interest and then starts to dematerialise, looking at you\nall the while...just before he fades out completely you hear him say",
+"\"Your opponent was worth @ points.\""
+}
+
